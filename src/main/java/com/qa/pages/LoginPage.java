53660fdd1e53c6c07d65e028c1f2a638b14bad8c
channu salgar <channu.chincholi@gmail.com>
2019-10-09 17:22:49 +0530

    first git commit

---
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,12 @@
+package com.qa.pages;
+
+public class LoginPage {
+
+	public void loginpage() {
+	System.out.println("login page");
+}
+
+	public void loginset() {
+		System.out.println("login set");
+	}
+}
